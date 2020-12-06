337662b870bf98d2e0f71d7ebfa3aeea6f0596c2
pashapushak1996 <pashapushak1996@gmail.com>
2020-12-06 15:06:15 +0200

    react clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import AllPosts from "./component/all-posts/AllPosts";
 
 class App extends Component {
     render() {
         return (
             <div>
-               <AllPosts/>
+                
             </div>
         );
     }
