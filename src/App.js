94f1b5a351af85bcc65d389d19311552f9bd1455
pashapushak1996 <pashapushak1996@gmail.com>
2020-12-08 00:10:56 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, {Component} from 'react';
-import AllUsers from "./components/all-users/AllUsers";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import AllPosts from "./components/all-posts/AllPosts";
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <Link to={'/users'}>
-                        users
-                    </Link>
-
-                    <Link to={'/posts'}>
-                        posts
-                    </Link>
-                    <Switch>
-                        <Route path={'/users'} render={() => {
-                            return <AllUsers/>;
-                        }}/>
-                        <Route path={'/posts'} render={()=>{
-                            return <AllPosts/>;
-                        }}/>
-                    </Switch>
-
-                </div>
-            </Router>
-        )
+            <div>
+                
+            </div>
+        );
     }
 }
 
